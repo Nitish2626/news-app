49b9736dee464eb4da4230ce07ac6e303b0aa958
Nitish Sharma <rsony8884@gmail.com>
2023-11-27 08:55:28 +0530

    App improved

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -8,8 +8,8 @@ const News = (props) => {
 
     const [articles, setArticles] = useState([]);
     const [loader, setLoader] = useState(false);
-    const [page,setPage]=useState(1);
-    const [totalResult,setTotalResult]=useState();
+    const [page, setPage] = useState(1);
+    const [totalResult, setTotalResult] = useState();
 
     let title = props.category.slice(1);
     document.title = props.category[0].toUpperCase() + title;
@@ -35,7 +35,7 @@ const News = (props) => {
             }
         };
         api();
-    }, [props.search,props.country,totalResult]);
+    }, [props.search, props.country, totalResult]);
 
     // const moreArticles=async ()=>{
     //     try {
@@ -56,18 +56,23 @@ const News = (props) => {
     // }
 
     return (
-        <div className="w-full flex items-center justify-center flex-wrap gap-10 pt-12 pb-5">
+        <>
+            <div className="w-full flex items-center justify-center flex-wrap gap-10 pt-12 pb-5">
 
-            {loader ? <Loader borderColor="border-blue-500" /> : articles.map((item, i) => {
-                return <NewsItem key={i} title={item.title} image={item.urlToImage} description={item.description} url={item.url} source={item.source.name} author={item.author} time={item.publishedAt} />
-            })}
+                {loader ? <Loader borderColor="border-blue-500" /> : articles.map((item, i) => {
+                    return <NewsItem key={i} title={item.title} image={item.urlToImage} description={item.description} url={item.url} source={item.source.name} author={item.author} time={item.publishedAt} />
+                })}
+
+            </div>
 
             <section className="flex items-center justify-end">
+
                 <Buttons text="Prev"></Buttons>
                 <Buttons text="Next"></Buttons>
+                
             </section>
 
-        </div>
+        </>
     );
 }
 export default News;
